7483413f8176b6afa0e1172a72615aec80508631
Nicolas Zander <118601323+ZanderNic@users.noreply.github.com>
2024-10-15 17:53:43 +0200

    Update WCTGan.py

--- src/Model/Gans/WCTGan.py
+++ src/Model/Gans/WCTGan.py
@@ -19,8 +19,8 @@ from ..Critic.critic import Discriminator
 from ...Data.dataset import CTGan_data_set
 from ..Extra_Pen.Classifier import Classifier
 from ._gan_utils import get_nolin_act, get_loss_function, format_time, format_time_with_h, gradient_penalty, wasserstein_loss
-from ...Data.encoder_condition import Cond_Encoder
-from ...Data.encoder_data import Data_Encoder
+from ...Data.data_encoder import DataEncoder
+
 
 
 
@@ -167,8 +167,8 @@ class WCTGan(Base_CTGan):
 
     def preprocess_and_load_data(self, ctgan_data_set, batch_size)-> DataLoader:
         data = ctgan_data_set.dataframe()
-        conditiond_encoded = self.cond_encoder.get_cond_from_data(data)
-        data_encoded = self.data_encoder(data)
+        conditiond_encoded = self.data_encoder.transform_cond(data)
+        data_encoded = self.data_encoder.transform_data(data)
 
         dataset = torch.utils.data.TensorDataset(
             data_encoded, 
@@ -182,8 +182,8 @@ class WCTGan(Base_CTGan):
         
         """
         data = ctgan_data_set.dataframe()
-        conditiond_encoded = self.cond_encoder.get_cond_from_data(data)
-        data_encoded = self.data_encoder(data)
+        conditiond_encoded = self.data_encoder.transform_cond(data)
+        data_encoded = self.data_encoder.transform_data(data)
 
         dataset = torch.utils.data.TensorDataset(
             data_encoded, 
@@ -201,8 +201,8 @@ class WCTGan(Base_CTGan):
 
     def preprocess_and_load_train_test_classifier(self, ctgan_data_set, batch_size, train_size = 0.8): #-> Tuple(torch.dataloader()): #TODO
         data = ctgan_data_set.dataframe()
-        conditiond_encoded = self.cond_encoder.get_cond_from_data(data)
-        data_encoded = self.data_encoder.transform_without_condition(data) 
+        conditiond_encoded = self.data_encoder.transform_cond(data)
+        data_encoded = self.data_encoder.transform_data_df_without_condition(data) 
 
         dataset = torch.utils.data.TensorDataset(
             data_encoded, 
@@ -243,14 +243,8 @@ class WCTGan(Base_CTGan):
         if not (self.generator and self.discriminator and self.classifier):
 
             self.cond_cols = ctgan_data_set.cond_cols()
-            self.cond_encoder = Cond_Encoder(
-                ctgan_data_set.dataframe(),
-                cond_cols=self.cond_cols,
-                categorical_columns=ctgan_data_set.cat_cols(),
-                numeric_columns=ctgan_data_set.num_cols(),
-                ordinal_columns=ctgan_data_set.ord_cols(),
-            )
-            self.data_encoder = Data_Encoder(
+            
+            self.data_encoder = DataEncoder(
                 ctgan_data_set.dataframe(),
                 cond_cols=self.cond_cols,
                 categorical_columns=ctgan_data_set.cat_cols(),
@@ -259,7 +253,7 @@ class WCTGan(Base_CTGan):
             )
 
             self.output_space =  self.data_encoder.encode_dim() 
-            self.n_units_conditional =  self.cond_encoder.condition_dim() 
+            self.n_units_conditional =  self.data_encoder.encode_cond_dim() 
 
             self.generator = self.generator_class(
                 generator_n_units_in= self.n_units_latent,
@@ -286,7 +280,7 @@ class WCTGan(Base_CTGan):
            
             self.classifier = Classifier(
                 classifier_n_units_in = self.output_space - self.n_units_conditional,
-                classifier_n_units_out_per_category = self.cond_encoder.get_units_per_column(),
+                classifier_n_units_out_per_category = self.data_encoder.get_units_per_cond_column(),
                 classifier_n_layers_hidden =  self.classifier_n_layers_hidden, 
                 classifier_n_units_hidden = self.classifier_n_units_hidden, 
                 classifier_nonlin= self.classifier_nonlin, 
@@ -341,8 +335,8 @@ class WCTGan(Base_CTGan):
                     X_fake = generator.forward(noise_input)
 
                     y_hat_real = discriminator(torch.cat([X_real], dim=1))
-                    y_hat_fake = discriminator(torch.cat([X_fake], dim=1)) # TODO vlt sollte der disc nicht nochmal auch die cond bekommen bei beiden beim einen ist sie ja schon dabei beim andern sollte er sich ja ned drum kümmern
-                    loss_critic = wasserstein_loss(y_hat_real, y_hat_fake)
+                    y_hat_fake = discriminator(torch.cat([X_fake], dim=1)) # TODO vlt sollte der disc nochmal auch die cond bekommen auch wenn sie schon dabei ist 
+                    loss_critic = - wasserstein_loss(y_hat_real, y_hat_fake)
 
                     gp = gradient_penalty(discriminator, torch.cat([X_real], dim=1), torch.cat([X_fake], dim=1), device) # Add gradient penalty 
                     loss_critic += lambda_gradient_penalty * gp
@@ -362,11 +356,12 @@ class WCTGan(Base_CTGan):
                     y_hat = discriminator(torch.cat([X_fake], dim=1))
                     loss_g = -torch.mean(y_hat)
 
-                    loss_cond = self.compute_condition_loss(X_fake.detach().cpu().numpy(), cond, X_real)   # Calculate condition loss and add to total loss
+                    loss_cond_class = self.compute_condition_classifier_loss(X_fake, cond)  
+                    loss_cond_match = self.compute_condition_loss(X_fake, cond) #TODO edit the way the cond is filtered from X_fake to mak it possible to backprob it 
 
-                    #cond_loss_weight = self.calculate_cond_loss_weight(epoch)
+                    loss_cond = loss_cond_match #(1 - self.lambda_condition_loss_weight) * loss_cond_match  + self.lambda_condition_loss_weight * loss_cond_class
                     
-                    total_loss = loss_g + self.lambda_condition_loss_weight * loss_cond    # total_loss = loss_g + self.calculate_cond_loss_weight(epoch) * loss_cond
+                    total_loss = loss_g + loss_cond # think if it makes sence to scale the loss depending on the epoch 
 
                     optim_generator.zero_grad()
                     total_loss.backward()
@@ -388,7 +383,6 @@ class WCTGan(Base_CTGan):
             if epoch % n_iter_print == 0:   
                 avg_time_per_epoch = total_time / epoch
                 remaining_time = avg_time_per_epoch * (n_epochs - epoch)
-
                 print(f"Epoch {epoch:4d} || Loss Critic: {epoch_loss_critic_train:7.4f} || Loss Gen: {epoch_loss_gen_train:7.4f} || Loss Cond: {cond_loss_gen_train:7.4f}|| Avg Time/Epoch: {format_time(avg_time_per_epoch)} || Remaining Time: {format_time_with_h(remaining_time)}")
 
             if n_iter_checkpoint and epoch % n_iter_checkpoint == 0:
@@ -466,7 +460,7 @@ class WCTGan(Base_CTGan):
 
 
     def save(self, save_path):
-        pass #TODO https://pytorch.org/tutorials/beginner/saving_loading_models.html save multible state dicts append condition coloumns, self.cond_encoder somehow seld.data_ecoder also somehow
+        pass #TODO https://pytorch.org/tutorials/beginner/saving_loading_models.html save multible state dicts append condition coloumns,  seld.data_ecoder  somehow
 
     def load(self, path):
         pass #TODO model.load_state_dict(torch.load(PATH, weights_only=True, map_location="cuda:0"))  # it is not so easy to load a model trained on cuda on your cpu 
